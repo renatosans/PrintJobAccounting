485ea3b85921bb67454cda60472ba20c4685dbc3
renatosans <renato@datacopy.com.br>
2017-08-27 11:45:19 -0300

    Alterações na solução

--- DatabaseScripts/AppCommon/CreateDatabase.sql
+++ DatabaseScripts/AppCommon/CreateDatabase.sql
@@ -1,16 +1,17 @@
 
-CREATE DATABASE AppCommon
-GO
+-- Script desabilitado por motivos de compatibilidade com o Microsoft Azure
+
+-- CREATE DATABASE AppCommon
+-- GO
 
 
-USE AppCommon
-GO
+-- USE AppCommon
+-- GO
 
 
 -- sp_grantdbaccess FrameworkUser
-CREATE USER FrameworkUser FOR LOGIN FrameworkUser
-GO
-sp_addrolemember db_datareader, FrameworkUser
-GO
-sp_addrolemember db_datawriter, FrameworkUser
-GO
+-- GO
+-- sp_addrolemember db_datareader, FrameworkUser
+-- GO
+-- sp_addrolemember db_datawriter, FrameworkUser
+-- GO
