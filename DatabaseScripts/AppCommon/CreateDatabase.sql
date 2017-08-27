80a7981a76727d9ef621faf6d40807a6f4204961
renatosans <renato@datacopy.com.br>
2017-08-27 11:52:18 -0300

    Alterações na solução

--- DatabaseScripts/AppCommon/CreateDatabase.sql
+++ DatabaseScripts/AppCommon/CreateDatabase.sql
@@ -1,5 +1,6 @@
 
 -- Script desabilitado por motivos de compatibilidade com o Microsoft Azure
+-- Todas as tabelas e procedures deste banco são transferidas para o banco Accounting pelo instalador do sistema
 
 -- CREATE DATABASE AppCommon
 -- GO
